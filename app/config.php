0fbc1640317fec4407902f87f189c83bcea4bcf8
sridattaga <sivag.soc95@gmail.com>
2026-04-25 13:13:33 +0530

    Add configuration file for environment variables

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_NAME', getenv('DB_NAME'));
+
+define('S3_BUCKET', getenv('S3_BUCKET'));
+?>
